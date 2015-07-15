3de48510fc0d54979c45343ceb57de1af5be0a90
davidkelley <david.james.kelley@gmail.com>
2015-07-15 12:51:49 +0100

    removing tests for now

--- spec/app/entities/sector_entity_spec.rb
+++
@@ -1,7 +0,0 @@
-describe SectorEntity do
-  subject { SectorEntity }
-
-  it { is_expected.to represent(:name) }
-
-  it { is_expected.to represent(:code) }
-end
--- spec/factories/sector.rb
+++
@@ -1,5 +0,0 @@
-FactoryGirl.define do
-  factory :sector do
-    initialize_with { ROM.env.relation(:sectors).as(:sector).children(50).to_a.sample }
-  end
-end
